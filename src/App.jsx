f7dd687028c3aa33ac83a7c4f0c9d90db505e8bb
Shahzad Khan <joykhan78600@gmail.com>
2026-08-22 21:02:27 +0530

    feat: add initial RB Corp route structure

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+import SiteLayout from "./components/layout/SiteLayout";
+import HomePage from "./pages/HomePage";
+import PlaceholderPage from "./pages/PlaceholderPage";
+
+function App() {
+  return (
+    <Routes>
+      <Route element={<SiteLayout />}>
+        <Route index element={<HomePage />} />
+        <Route path="about" element={<PlaceholderPage title="About RB Corp" />} />
+        <Route path="companies" element={<PlaceholderPage title="Our Companies" />} />
+        <Route path="companies/beyonist" element={<PlaceholderPage title="Beyonist" />} />
+        <Route path="companies/rb-service-connect" element={<PlaceholderPage title="RB Service Connect" />} />
+        <Route path="companies/rb-finance" element={<PlaceholderPage title="RB Finance" />} />
+        <Route path="companies/royalties-charity" element={<PlaceholderPage title="Royalties Charity" />} />
+        <Route path="companies/royalties-buffet" element={<PlaceholderPage title="Royalties Buffet" />} />
+        <Route path="impact" element={<PlaceholderPage title="Impact" />} />
+        <Route path="insights" element={<PlaceholderPage title="Insights" />} />
+        <Route path="careers" element={<PlaceholderPage title="Careers" />} />
+        <Route path="contact" element={<PlaceholderPage title="Contact" />} />
+      </Route>
+      <Route path="*" element={<Navigate to="/" replace />} />
+    </Routes>
+  );
+}
+
+export default App;

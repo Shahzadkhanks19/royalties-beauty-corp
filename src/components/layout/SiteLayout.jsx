19cbe551e42189cf50fd6e922322a8c614c36d87
Shahzad Khan <joykhan78600@gmail.com>
2026-08-23 13:33:47 +0530

    Reset scroll position on route changes

--- src/components/layout/SiteLayout.jsx
+++ src/components/layout/SiteLayout.jsx
@@ -1,9 +1,16 @@
-import { Outlet } from "react-router-dom";
+import { useEffect } from "react";
+import { Outlet, useLocation } from "react-router-dom";
 import ScrollToTopButton from "../ui/ScrollToTopButton";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 
 function SiteLayout() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, left: 0, behavior: "auto" });
+  }, [pathname]);
+
   return (
     <div className="min-h-screen bg-[#f7f4f0] text-[#151517]">
       <Navbar />

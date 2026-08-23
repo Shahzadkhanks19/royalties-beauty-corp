54b090eacda1b7d2623eccdc6c2826acecaf1b4d
Shahzad Khan <joykhan78600@gmail.com>
2026-08-23 14:42:31 +0530

    Add global error boundary and initial preloader

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import GlobalErrorBoundary from "./components/system/GlobalErrorBoundary";
+import Preloader from "./components/system/Preloader";
 import "./styles/index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <GlobalErrorBoundary>
+      <BrowserRouter>
+        <Preloader />
+        <App />
+      </BrowserRouter>
+    </GlobalErrorBoundary>
   </React.StrictMode>,
 );
